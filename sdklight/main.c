b8b120e8dc2547f124a89950d7c5343429d20312
Juliet <youchjiang@gmail.com>
2026-05-20 17:26:41 +0800

    add a c version

---
+++ sdklight/main.c
@@ -0,0 +1,9 @@
+
+#include "pico/stdlib.h"        // include pico-sdk 的标准库头文件
+
+int main() {
+    gpio_init(15);              // 初始化 GPIO15
+    gpio_set_dir(15, GPIO_OUT); // 设置为 输出模式
+    gpio_put(15, 1);            // 输出高电平
+    while (1);                  // 死循环
+}
